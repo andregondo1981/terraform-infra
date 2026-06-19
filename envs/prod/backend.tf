2e761ed3441951242ec2e215eb4d303200c693a0
Andre <andre1981@gmail.com>
2026-06-19 19:26:46 -0400

    feat(infra): add resource for prod closes ea-5678 terraform plan output is in the comment

---
+++ envs/prod/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket = "s3-bucket-utrains"
+    key    = "envs/prod/terraform.tfstate"
+    region = "us-east-1"    
+    use_lockfile = true
+
+  }
+}
---
+++ envs/prod/main.tf
@@ -0,0 +1,6 @@
+module vpc {
+  source = "../../modules/vpc"
+
+  vpc_cidr = "170.120.0.0/16"
+  vpc_name = "prod-portal-vpc"
+}
